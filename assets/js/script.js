7f663423ffc0cb49add2e121518254c50f87794a
unknown <ebussman@gmail.com>
2022-10-09 15:01:02 -0700

    Added animation css, added a pageload fade in animation for both pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, options);
-  });
+let submitButton = $('#submitBtn');
+let firstName = $('#first_name');
+let userMotivator = $('#motivator');
+let userInfo = {};
+
+
+
+submitButton.on('click', function() {
+    console.log("clicked");
+
+
+    let userInfo ={
+        name: firstName.val(),
+        motivator: userMotivator.val()
+    };
+
+    localStorage.setItem("User", JSON.stringify(userInfo));
+
+    window.location.assign("file:///C:/Users/Erik/code/Project1-Prototype/pages/hobby.html");
+
+
+});
+
+// When the submit button is clicked, your name and motivator are saved
+// submitButton.addEventListener('click', saveUserInfo());
+
