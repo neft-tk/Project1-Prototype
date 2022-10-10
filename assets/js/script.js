2a9bf77b648d8872164de5240338cd9c85bbb623
unknown <ebussman@gmail.com>
2022-10-10 09:25:18 -0700

    Added text form to modal with white background

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,37 +34,47 @@ function loadUserData() {
     document.querySelector('#userNameHere').textContent = "Welcome, " + loadUser.name;
 };
 
-    
+// Launches welcome statement
 loadUserData();
 
 
-
-taskButton.on('click', function() {
+// TODO: Create a card for task(s)
+function createCard() {
     console.log("taskbutton clicked");
     let div = document.createElement("div");
-    div.setAttribute("class", "container");
+    let h3 = document.createElement("h3");
+
+
+    div.setAttribute("class", "task container z-depth-3");
+    h3.innerText = tas
+    
     
     listedTasks.append(div);
     
 
-});
+};
 
-
+// Modal function
 $(document).ready(function() {
     $('.modal').modal();
     $('.parallax').parallax();
 });
 
+// Launches the modal window
 function toggleModal() {
     var instance = M.Modal.getInstance($('#modal3'))
     instance.open();
 };
 
+
+
 // TODO: grabbing an option from the modal
 
+// Creates an object that CURRENTLY locally stores the most recent input value
 function createTask() {
 
     let taskDetails = {
+        taskName: document.querySelector(''),
         exp: document.querySelector('#difficultySelect').value,
         subtasks: document.querySelector('#subtaskSelect').value
     };
@@ -72,4 +82,6 @@ function createTask() {
     localStorage.setItem("Task", JSON.stringify(taskDetails));
   
     console.log(taskDetails);
+
+    createCard();
 };
