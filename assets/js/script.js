f614cb2f3ebbfda13e654fedc2e49eadf0dfa313
unknown <ebussman@gmail.com>
2022-10-09 19:48:14 -0700

    Working dropdown boxes that successfully save user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,11 @@ let userInfo = {};
 
 // Hobby Page Variables
 let userNameHere = $('#userNameHere');
+let taskButton = $('#taskBtn');
+let listedTasks = $('#taskList');
 
 
-
+// Stores the user's info locally and then changes the page to the main hobby tracker page
 submitButton.on('click', function() {
     console.log("clicked");
 
@@ -24,6 +26,8 @@ submitButton.on('click', function() {
 
 });
 
+
+// On page load inserts a welcome message for the user based on the name they stored locally
 function loadUserData() {
     let loadUser = JSON.parse(localStorage.getItem("User"));
     console.log(loadUser.name);
@@ -33,6 +37,54 @@ function loadUserData() {
     
 loadUserData();
 
-// When the submit button is clicked, your name and motivator are saved
-// submitButton.addEventListener('click', saveUserInfo());
 
+
+taskButton.on('click', function() {
+    console.log("taskbutton clicked");
+    let div = document.createElement("div");
+    div.setAttribute("class", "container");
+    
+    listedTasks.append(div);
+    
+
+});
+
+
+$(document).ready(function() {
+    $('.modal').modal();
+    $('.parallax').parallax();
+});
+
+function toggleModal() {
+    var instance = M.Modal.getInstance($('#modal3'))
+    instance.open();
+};
+
+// TODO: grabbing an option from the modal
+
+function createTask() {
+
+    let taskDetails = {
+        exp: document.querySelector('#difficultySelect').value,
+        subtasks: document.querySelector('#subtaskSelect').value
+    };
+
+
+    // function getDifficultyOption() {
+    //     selectElement = document.querySelector('#difficultySelect');
+    //     output = selectElement.options[selectElement.selectedIndex].value;
+    //     taskDetails.exp.textContent = output;
+    // };
+
+    // function getSubTasksOption() {
+    //     selectElement = document.querySelector('#subtaskSelect');
+    //     output = selectElement.options[selectElement.selectedIndex].value;
+    //     taskDetails.subtasks.textContent = output;
+    // };
+
+    localStorage.setItem("Task", JSON.stringify(taskDetails));
+
+    // getDifficultyOption();
+    // getSubTasksOption();   
+    console.log(taskDetails);
+};
